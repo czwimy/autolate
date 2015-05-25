6ce01cb3b437b555280969e80c87ace35ea28f19
czwimy <czwimy@163.com>
2015-05-25 18:15:49 +0800

    autotext
    新的text

---
+++ autolate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autolate
+//
+//  Created by 陈志伟 on 15/5/25.
+//  Copyright (c) 2015年 czw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
